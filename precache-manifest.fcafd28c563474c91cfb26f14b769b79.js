60ba610453a73eb5a0dd5f08ba95fa4a1f0c6053
Shashank Singh <singhshashankcic@ducic.ac.in>
2019-09-20 10:50:27 +0530

    Updates

---
+++ precache-manifest.fcafd28c563474c91cfb26f14b769b79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deea97c3c6558e01b649998dee733799",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "1e13e9734915f9d0d408",
+    "url": "/social-network/static/css/main.b3f20669.chunk.css"
+  },
+  {
+    "revision": "144f4bafcd5764496d9c",
+    "url": "/social-network/static/js/2.8cdac602.chunk.js"
+  },
+  {
+    "revision": "1e13e9734915f9d0d408",
+    "url": "/social-network/static/js/main.57cbba24.chunk.js"
+  },
+  {
+    "revision": "6a814cf7f1f82e5411f3",
+    "url": "/social-network/static/js/runtime-main.fadc0544.js"
+  },
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/social-network/static/media/no-img.6732bd42.png"
+  }
+]);
